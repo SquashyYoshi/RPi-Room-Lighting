7b5b6e175c9478c91cdb10436d2b01142f9f46ee
SquashyYoshi <joshua.smith722@gmail.com>
2024-12-24 13:14:00 -0600

    Tried to manually emulate one of remotes signals. Didn't work

---
+++ remote_codes/remote_codes.ino
@@ -0,0 +1,46 @@
+const int TX_PIN = 12;
+//up for x ms, down for x ms...
+short lightCode[] = {40, 20, 40, 20, 40, 20, 40, 200, 280};
+short delaysLen = 9;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(TX_PIN, OUTPUT);
+}
+
+void loop() 
+{
+    
+    bool curState = true;
+    for(int i = 0; i < delaysLen; i++) {
+        digitalWrite(TX_PIN, curState);
+        Serial.print(">tx:");
+        Serial.print(curState);
+        Serial.print("\r\n");
+
+        Serial.print(">time:");
+        Serial.print(millis() / 1000.0);
+        Serial.print("\r\n");
+        Serial.println();
+
+        curState = !curState;
+        delay(lightCode[i]);
+
+        
+    }
+    digitalWrite(TX_PIN, false);
+    Serial.print(">tx:");
+    Serial.print(false);
+    Serial.print("\r\n");
+
+    Serial.print(">time:");
+    Serial.print(millis() / 1000.0);
+    Serial.print("\r\n");
+
+    Serial.print("\n\n_____WAITING_______\n\n");
+    delay(5000);
+    
+
+}
+
