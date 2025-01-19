05d2c2cfc46f988e30900fe7807d4c1ec5bacfb0
SquashyYoshi <joshua.smith722@gmail.com>
2025-01-19 09:36:18 -0600

    Added counter of signals received

--- sketches/rf/test-sniff/test-sniff.ino
+++ sketches/rf/test-sniff/test-sniff.ino
@@ -1,23 +1,25 @@
 #include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
-
+int signalsSoFar = 0;
 void setup() {
-  Serial.begin(9600);
-  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+    Serial.begin(9600);
+    mySwitch.enableReceive(1);  
+    Serial.println("ready for signals...");
 }
 
 void loop() {
-  if (mySwitch.available()) {
-    
-    Serial.print("Received ");
-    Serial.print( mySwitch.getReceivedValue() );
-    Serial.print(" / ");
-    Serial.print( mySwitch.getReceivedBitlength() );
-    Serial.print("bit ");
-    Serial.print("Protocol: ");
-    Serial.println( mySwitch.getReceivedProtocol() );
+    if (mySwitch.available()) {
+        signalsSoFar++;
+        Serial.println(signalsSoFar);
+        Serial.print("Received ");
+        Serial.print( mySwitch.getReceivedValue() );
+        Serial.print(" / ");
+        Serial.print( mySwitch.getReceivedBitlength() );
+        Serial.print("bit ");
+        Serial.print("Protocol: ");
+        Serial.println( mySwitch.getReceivedProtocol() );
 
-    mySwitch.resetAvailable();
-  }
+        mySwitch.resetAvailable();
+    }
 }
