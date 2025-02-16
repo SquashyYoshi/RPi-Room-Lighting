9bdfacb9f5ccbee6d956d71af78b667d67de5b9c
SquashyYoshi <joshua.smith722@gmail.com>
2025-02-16 10:07:26 -0600

    Communicates with ard using checksums

--- pi/rf-test.py
+++ pi/rf-test.py
@@ -2,25 +2,31 @@ import rpi_rf
 import RPi.GPIO as GPIO
 import time
 import threading
+from datetime import datetime, timedelta
 
 
 def main():
-    #start()
-    re = rpi_rf.RFDevice(10, rx_tolerance=0) #gpio pin number
+    global re
+    global tr
+    global canReceive 
+    canReceive = True
+    re = rpi_rf.RFDevice(10) #gpio pin number rx_tolerance=0 (might add that parameter back)
     tr = rpi_rf.RFDevice(2)
     re.enable_rx()
     print("rX enabled")
     tr.enable_tx()
     print("tX enabled")
+
     try:
-        inThread = threading.Thread(target=usrSend)
-        inThread.daemon = True
-        inThread.start()
-        while True:
-            if re.rx_code:
-                print(f"received {re.rx_code}")
-                re.rx_code = None
-            time.sleep(0.2)
+        usrSend()
+        # txThread = threading.Thread(target=usrSend)
+        # txThread.daemon = True
+        # txThread.start()
+        # while True:
+        #     if canReceive and re.rx_code:
+        #         print(f"received {re.rx_code}")
+        #         re.rx_code = None
+        #     time.sleep(0.2)
     except KeyboardInterrupt:
         print()
 
@@ -28,16 +34,23 @@ def main():
 def usrSend():
     while True:
         val = int(input())
+        checkSum = int(val/2 + 8)
+        success = False
+        canReceive = False
         tr.tx_code(val)
-        time.sleep(0.2)
-def start():
-    global re, tr
-    re = rpi_rf.RFDevice(10, rx_tolerance=0) #gpio pin number
-    tr = rpi_rf.RFDevice(2)
-    re.enable_rx()
-    print("rX enabled")
-    tr.enable_tx()
-    print("tX enabled")
+        print(f"Should hear {checkSum}...")
+        startTime = datetime.now()
+        while(datetime.now() < startTime + timedelta(seconds=3)):
+            if re.rx_code == checkSum:
+                print("Checksum received")
+                success = True
+                re.rx_code = None
+            time.sleep(0.1)
+        if not success:
+            print("Checksum not received after 3 seconds ;(")
+        canReceive = True
+
+
 main()
 
 # print("uhhh there was something")
